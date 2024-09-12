eb5a33c1b7495e967b32ea42d6e3a6f8dee0c949
Mukundakrishnan <92831916+Mukundanks@users.noreply.github.com>
2024-09-12 13:11:13 +0100

    assignment
    A program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty-two. Otherwise output No.

---
+++ deep.py
@@ -0,0 +1,7 @@
+ans = input("What is the answer to the greatest question of Life, the Universe and Everything? ")
+ans = ans.lower().strip()
+if ans == "42" or ans =="forty-two" or ans == "forty two":
+    print("Yes")
+else:
+    print("No")
+
